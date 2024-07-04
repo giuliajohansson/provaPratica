e90c8e98713f590dbaab8f8d8e29063b08b8f939
giuliajohansson <giulia_rodrigues@estudante.sesisenai.org.br>
2024-07-04 09:34:22 -0300

    clubes

---
+++ src/App.jsx
@@ -0,0 +1,15 @@
+import './App.css'
+import Header from './components/Header'
+import Clubes from './components/Clubes'
+
+function App() {
+
+    return (
+      <div className='paginaLista'>
+        <Header />
+        <Clubes />
+        </div>
+    )
+  }
+
+export default App
---
+++ src/components/Header.jsx
@@ -0,0 +1,10 @@
+import React from "react";
+
+export default function Header() {
+    return (
+        <header className="header">
+            <img className= 'imagemLogo' src='./senailogo.png'/>
+            <h1>Giulia Johansson Rodrigues - 3B</h1>
+        </header>
+    );
+}
